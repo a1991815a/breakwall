2e3031cea81d187c18b470095383ed1ce303ae05
1991815555@163.com <1991815555@163.com>
2017-09-13 00:11:41 +0800

    master

--- init.sh
+++ init.sh
@@ -1,100 +1,113 @@
 #! /bin/sh
+stty -echo
+
+#function define
 function read_default()
 {
-    read -p "$1[default: $2]" read_val
+    read -p "$1[default: $2]:  " read_val
     if [ -z "$read_val" ]; then
         read_val=$2
     fi
     return $read_val
 }
 
-yum update -y
-yum install -y vim git httpd firewalld wget openssl python tar bzip2 gzip
-yum install -y python-setuptools && easy_install pip
-pip install git+https://github.com/shadowsocks/shadowsocks.git@master
-pip install supervisor
+function read_pwd()
+{
+    read -p "$1[default: 88888888]:  " -s read_val
+    if [ -z "$read_val" ]; then
+    	read_val="88888888"
+	fi
+    return $read_val
+}
 
-mkdir /tmp/tmp
-pushd /tmp/ss-kcptun && rm -frd *
+function check_install()
+{
+	which $1 || return ""
+	return "$2 "
+}
 
-latestUrl=https://github.com$(wget -O- https://github.com/xtaci/kcptun/releases/latest | grep -P 'href=[\s\S]*kcptun-linux-amd64[\s\S]*' | cut -d'"' -f2 | cut -d'"' -f1)
-wget -O- $latestUrl
-
-rm -frd /usr/kcptun
-mkdir /usr/kcptun
-rm -frd /usr/shadowsocks
-mkdir /usr/shadowsocks
-
-tar -xf kcptun-linux-amd64* && mv server_linux_amd64 /usr/kcptun/kcp-server && ln -s /usr/kcptun/kcp-server /usr/bin/kcptun
-
-shadowsocks_bin=$(which ssserver || (echo "ssserver install failure..."; exit 1))
-kcptun_bin=$(which kcptun || (echo "kcptun install failure..."; exit 1))
-supervisord_bin=$(which supervisord && which echo_supervisord_conf || (echo "supervisord install failure..."; exit 1))
-
+#read custom setting from user input
 shadowsocks_port=$(read_default "shadowsocks_port" 8388)
 shadowsocks_localport=$(read_default "shadowsocks_localport" 1080)
-shadowsocks_localport=$(read_default "shadowsocks_localport" 1080)
-shadowsocks_localport=$(read_default "kcptun_port" 29900)
-shadowsocks_localport=$(read_default "kcptun_pwd" 123123)
+shadowsocks_pwd=$(read_pwd "shadowsocks_pwd")
+kcptun_port=$(read_default "kcptun_port" 29900)
+kcptun_pwd=$(read_pwd "kcptun_pwd")
+supervisord_username=$(read_default "supervisord_username" "myuser")
+supervisord_passwd=$(read_pwd "supervisord_passwd")
 
-wget -O- https://raw.githubusercontent.com/a1991815a/breakwall/master/kcptun.json > /etc/kcptun.json
-wget -O- https://raw.githubusercontent.com/a1991815a/breakwall/master/shadowsocks.json > /etc/shadowsocks.json
+#check and install base package and util
+install_package=$(check_install git git)
+install_package=$(check_install httpd httpd)
+install_package=$(check_install firewalld firewalld)
+install_package=$(check_install wget wget)
+install_package=$(check_install openssl openssl)
+install_package=$(check_install python python)
+install_package=$(check_install tar tar)
+install_package=$(check_install bzip2 bzip2)
+install_package=$(check_install gzip gzip)
+install_package=$(check_install easy_install python-setuptools)
 
+yum update -y || exit 1
 
-read -p "supervisord control username: " username
-read -p "supervisord control password: " -s password
+if [ -n "$install_package" ]; then
+	yum install -y $install_package || exit 1
+fi
 
+which pip || easy_install pip || exit 1
+which ssserver || pip install git+https://github.com/shadowsocks/shadowsocks.git@master || exit 1
+which supervisord || pip install supervisor || exit 1
 
-newline=$'\n'
+#kcptun bin update or install
+rm -frd /tmp/ss-kcptun; mkdir /tmp/ss-kcptun
+pushd /tmp/ss-kcptun
+latestUrl=https://github.com$(wget -O- https://github.com/xtaci/kcptun/releases/latest | grep -P 'href=[\s\S]*kcptun-linux-amd64[\s\S]*' | cut -d'"' -f2 | cut -d'"' -f1)
+wget $latestUrl
+rm -frd /usr/kcptun; mkdir /usr/kcptun;
+tar -xf kcptun-linux-amd64* && mv server_linux_amd64 /usr/kcptun/kcp-server && ln -s /usr/kcptun/kcp-server /usr/bin/kcptun
+popd
+rm -frd /tmp/ss-kcptun
 
-supervisord_script=""
-supervisord_script+="[unix_http_server]$newline"
-supervisord_script+="file=/tmp/supervisor.sock$newline"
-supervisord_script+="chmod=0700$newline"
-supervisord_script+="chown=root:root$newline"
-supervisord_script+="username=$username$newline"
-supervisord_script+="password=$password$newline"
+#shadowsocks kcptun config
+shadowsocks_bin=$(which ssserver || (echo "ssserver install failure..."; exit 1))
+kcptun_bin=$(which kcptun || (echo "kcptun install failure..."; exit 1))
+supervisord_bin=$((which supervisord && which echo_supervisord_conf) || (echo "supervisord install failure..."; exit 1))
 
-supervisord_script+="[inet_http_server]$newline"
-supervisord_script+="port=0.0.0.0:9001$newline"
-supervisord_script+="username=$username$newline"
-supervisord_script+="password=$password$newline"
+(wget -O- https://raw.githubusercontent.com/a1991815a/breakwall/master/kcptun.json | sed "s/%kcptun_port%/$kcptun_port/g" | sed "s/%shadowsocks_port%/$shadowsocks_port/g" | sed "s/%kcptun_pwd%/$kcptun_pwd/g") > /etc/kcptun.json
+(wget -O- https://raw.githubusercontent.com/a1991815a/breakwall/master/shadowsocks.json | sed "s/%shadowsocks_port%/$shadowsocks_port/g" | sed "s/%shadowsocks_localport%/$shadowsocks_localport/g" | sed "s/%shadowsocks_pwd%/$shadowsocks_pwd/g") > /etc/shadowsocks.json
 
-supervisord_script+="[program:shadowsocks]$newline"
-supervisord_script+="command=$shadowsocks_bin -c /etc/shadowsocks.json$newline"
-supervisord_script+="process_name=%(program_name)s$newline"
-supervisord_script+="numprocs=1$newline"
-supervisord_script+="directory=/usr/shadowsocks$newline"
-supervisord_script+="umask=022$newline"
-supervisord_script+="priority=999$newline"
-supervisord_script+="autostart=true$newline"
-supervisord_script+="startsecs=1$newline"
-supervisord_script+="startretries=3$newline"
-supervisord_script+="autorestart=true$newline"
-supervisord_script+="stopwaitsecs=10$newline"
-supervisord_script+="user=root$newline"
-
-supervisord_script+="[program:kcptun]$newline"
-supervisord_script+="command=$kcptun_bin -c /etc/kcptun.json$newline"
-supervisord_script+="process_name=%(program_name)s$newline"
-supervisord_script+="numprocs=1$newline"
-supervisord_script+="directory=/usr/kcptun$newline"
-supervisord_script+="umask=022$newline"
-supervisord_script+="priority=998$newline"
-supervisord_script+="autostart=true$newline"
-supervisord_script+="startsecs=1$newline"
-supervisord_script+="startretries=3$newline"
-supervisord_script+="autorestart=true$newline"
-supervisord_script+="stopwaitsecs=10$newline"
-supervisord_script+="user=root$newline"
-
-echo_supervisord_conf > /etc/supervisord.conf
-after_line=$(cat /etc/supervisord.conf | grep -n unix_http_server | head -n 1 | cut -d ":" -f1)
-after_line=$[after_line + 7]
-
-echo $(cat /etc/supervisord.conf | sed -n "$after_line,\$p") >/etc/supervisord.conf
+#supervisord config
+supervisord_script = $(wget -O- https://raw.githubusercontent.com/a1991815a/breakwall/master/supervisord.conf | sed "s/%username%/$supervisord_username/g" | sed "s/%passwd%/$supervisord_passwd/g" | sed "s/%shadowsocks_bin%/$shadowsocks_bin/g" | sed "s/%kcptun_bin%/%kcptun_bin/g")
 
 echo $supervisord_script >>/etc/supervisord.conf
 
+if [ -n "$(cat /etc/rc.local | grep supervisord)" ]; then
+	echo "supervisord -c /etc/supervisord.conf" >> /etc/rc.local
+fi
+
+#firewalld setting
+firewall-cmd --remove-service=shadowsocks --permanent
+firewall-cmd --remove-service=kcptun --permanent
+firewall-cmd --remove-service=supervisord --permanent
+
+firewall-cmd --delete-service=shadowsocks --permanent
+firewall-cmd --delete-service=kcptun --permanent
+firewall-cmd --delete-service=supervisord --permanent
+
 firewall-cmd --new-service=shadowsocks --permanent
-firewall-cmd --service=shadowsocks --add-port= --permanent
+firewall-cmd --new-service=kcptun --permanent
+firewall-cmd --new-service=supervisord --permanent
+
+firewall-cmd --service=shadowsocks --add-port=$shadowsocks_port/tcp --permanent
+firewall-cmd --service=shadowsocks --add-port=$shadowsocks_port/udp --permanent
+
+firewall-cmd --service=kcptun --add-port=$kcptun_port/udp --permanent
+
+firewall-cmd --service=supervisord --add-port=9001/tcp --permanent
+
+firewall-cmd --add-service=shadowsocks --permanent
+firewall-cmd --add-service=kcptun --permanent
+firewall-cmd --add-service=supervisord --permanent
+firewall-cmd --add-service=http --permanent
+firewall-cmd --add-service=https --permanent
+
+stty echo
